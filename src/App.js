c8a77fbb05c11dab2b357c19f224dd5ecd80d5d4
medampadma <medampadma1476@gmail.com>
2021-10-05 16:19:10 +0530

    backend data

--- src/App.js
+++ src/App.js
@@ -1,23 +1,38 @@
-import logo from './logo.svg';
-import './App.css';
+import react, { useState } from "react";
+import axios from 'axios';
 
 function App() {
+  const [search, setSearch] = useState("");
+  const [data, setData]=  useState([]);
+  const YOUR_APP_ID= '1943331e';
+   const YOUR_APP_KEY='2c260fcfc0c5bcdd978dd58fe9882404';
+
+  const submitHandler = (e) => {
+    
+    setSearch(e.target.value)
+    console.log(search);
+    e.preventDefault(); 
+    axios.get(`https://api.edamam.com/search?q=${search}&app_id=${YOUR_APP_ID}&app_key=${YOUR_APP_KEY}&from=0&to=30&calories=591-722&health=alcohol-free`)
+    .then(data=>setData(data.hits))
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <center>
+        <h4>Food Recipe App </h4>
+        <form>
+          <input
+            type="text"
+            value={search}
+            onChange={submitHandler}
+          />
+          <h1>{search}</h1>
+          <br />
+          <input type="submit" value="Search" className="btn btn-primary"/> <br />
+        </form>
+    
+
+      </center>
     </div>
   );
 }
